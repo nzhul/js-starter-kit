c8b1622936d2e0c03ccf173fd50447f0dc14f032
Dobromir Ivanov <dobromirivanov1@gmail.com>
2017-05-22 14:48:22 +0300

    adding linting to the project

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,6 +4,7 @@ import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
+/* eslint-disable no-console */
 
 const port = 3000;
 const app = express();
@@ -22,6 +23,6 @@ app.listen(port, function(err){
 	if(err){
 		console.log(err);
 	} else{
-		open('http://localhost:' + port); 
+		open('http://localhost:' + port);
 	}
 });
